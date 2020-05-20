449947d0172d31db4527b2948cc6e439c002ac73
Jairo <jairopereira_dias@hotmail.com>
2020-05-20 06:41:29 -0300

    decidido fazer a mão os botões de previous e net para melhor visualização de layout, desabilitei o search e show por não gostar do estilo

--- base/gerente.js
+++ base/gerente.js
@@ -1,6 +1,6 @@
 var buttonTeste = document.querySelector(".btn-menu");
 var info = document.querySelector('.col-vendas');
-// var vendas = document.querySelector('.info-vendas');
+var text = document.querySelector('.modulo');
 var toggle = 0;
 
 buttonTeste.addEventListener("click", event => {
@@ -9,12 +9,14 @@ buttonTeste.addEventListener("click", event => {
     buttonTeste.setAttribute("style", "margin-left: 15%");
     document.querySelector('.menu').setAttribute("style","left: 0px !important");
     info.setAttribute("style", "margin-left: 10%");
+    text.setAttribute("style", "margin-left: 15%");
     toggle = 1;
   }else{
     toggle = 0;
     buttonTeste.setAttribute("style", " margin-left: 1%; margin-top: 1%;");
     document.querySelector('.menu').setAttribute("style","left: -300px");
-    info.setAttribute("style", "margin-left: 0px");
+    info.setAttribute("style", "margin-left: 0");
+    text.setAttribute("style", "margin-left: 0");
 
   }
   
