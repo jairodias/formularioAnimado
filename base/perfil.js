7e47806a94802a577489de7012992a393452b2e0
Jairo <jairopereira_dias@hotmail.com>
2020-05-21 18:46:05 -0300

    salvando a nova funcionalidade de cadastro de funcionarios

---
+++ base/perfil.js
@@ -0,0 +1,20 @@
+var buttonTeste = document.querySelector(".btn-menu");
+var info = document.querySelector('.col-vendas');
+var toggle = 0;
+
+buttonTeste.addEventListener("click", event => {
+  event.preventDefault();
+  if(toggle == 0){
+    buttonTeste.setAttribute("style", "margin-left: 15%");
+    document.querySelector('.menu').setAttribute("style","left: 0px !important");
+    info.setAttribute("style", "margin-left: 10%");
+    toggle = 1;
+  }else{
+    toggle = 0;
+    buttonTeste.setAttribute("style", " margin-left: 1%; margin-top: 1%;");
+    document.querySelector('.menu').setAttribute("style","left: -300px");
+    info.setAttribute("style", "margin-left: 0");
+
+  }
+  
+});
